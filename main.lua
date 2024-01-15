011e533c1e42cbd45a88a03573e45f8aed59a657
Alessandro Livio <92685009+Dissident11@users.noreply.github.com>
2024-01-15 21:30:08 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,28 +1,32 @@
-local screen_width = pesto.window.getWidth()
-local screen_height = pesto.window.getHeight()
-local tile_size = 20
-local window_width = screen_width / tile_size
-local window_height = screen_height / tile_size
-local border_space = 1
-local cells_number = (window_width - 2 * border_space) * (window_height - 2 * border_space)
+--screen variables
+local screenWidth = pesto.window.getWidth()
+local screenHeight = pesto.window.getHeight()
+local tileSize = 20
+--dimensions of window in tiles
+local windowWidth = screenWidth / tileSize
+local windowHeight = screenHeight / tileSize
+local borderSpace = 1 --how many tiles are removed from the border
+local cellsNumber = (windowWidth - 2 * borderSpace) * (windowHeight - 2 * borderSpace) --how many tiles/cells are created
 
-local tile_x = 3
-local tile_y = 3
-
-local visited_cells = {}
-local last_cells = {}
+--starting settings
+--coordinates from which the maze began to be drawn
+local tileX = math.random(windowWidth - 2 * borderSpace)
+local tileY = math.random(windowHeight - 2 * borderSpace)
+local visitedCells = {}
+local lastCells = {}
 local lines = {}
-local draw_lines = {}
+local toDrawLines = {}
 local borders = {}
+local borderThickness = 2
+local bordersMade = false
+local showLines = true
 
-local border_thickness = 2
-local borders_made = false
+local pause = false
 
 math.randomseed(os.time())
-local show_lines = true
 
 --draw unfilled rectangles with thickness of 1
-local function line_rectangle(x, y, width, height)
+local function lineRectangle(x, y, width, height)
     pesto.graphics.rectangle(x, y, width, 1)
     pesto.graphics.rectangle(x + width - 1, y, 1, height)
     pesto.graphics.rectangle(x, y, 1, height)
@@ -66,177 +70,222 @@ end
 
 function pesto.update(dt)
 
-    if #visited_cells < cells_number then
+    if not pause then
 
-        local current_cell = {tile_x, tile_y}
+        if #visitedCells < cellsNumber then --the maze is created till all the tiles have been used
 
-        if not tableContains(visited_cells, current_cell) then
-            table.insert(visited_cells, current_cell)
-        end
-        if not tableContains(last_cells, current_cell) then
-            table.insert(last_cells, current_cell)
-        end
+            local currentCell = {tileX, tileY}
 
-        local directions = {} --directions: 1 up, 2 right, 3 down, 4 left
-
-        --add directions based on the coordinates and near cells
-        if current_cell[1] ~= 1 and --add left direction if the cell doesn't touch the left border
-                not tableContains(visited_cells, {tile_x - 1, tile_y}) then --and there is no cell on the left
-            table.insert(directions, 4)
-        end
-        if current_cell[1] ~= window_width - 2 * border_space and --add right direction if the cell doesn't touch the right border
-                not tableContains(visited_cells, {tile_x + 1, tile_y}) then --and there is no cell on the right
-            table.insert(directions, 2)
-        end
-        if current_cell[2] ~= 1 and --add up direction if the cell doesn't touch the top border
-                not tableContains(visited_cells, {tile_x, tile_y - 1}) then --and there is no cell upper
-            table.insert(directions, 1)
-        end
-        if current_cell[2] ~= window_height - 2 * border_space and --add down direction if the cell doesn't touch the bottom border
-                not tableContains(visited_cells, {tile_x, tile_y + 1}) then --and there is no cell lower
-            table.insert(directions, 3)
-        end
-
-        if #directions > 0 then
-
-            local choosen_dir = math.random(#directions)
-            local new_dir = directions[choosen_dir]
-
-            local new_tile_x, new_tile_y
-            local new_line, new_tile
-            local x1, y1, x2, y2
-
-            if new_dir == 1 then
-                new_tile_y = tile_y - 1
-                new_tile_x = tile_x
-            elseif new_dir == 2 then
-                new_tile_x = tile_x + 1
-                new_tile_y = tile_y
-            elseif new_dir == 3 then
-                new_tile_y = tile_y + 1
-                new_tile_x = tile_x
-            elseif new_dir == 4 then
-                new_tile_x = tile_x - 1
-                new_tile_y = tile_y
+            if not tableContains(visitedCells, currentCell) then
+                table.insert(visitedCells, currentCell)
+            end
+            if not tableContains(lastCells, currentCell) then
+                table.insert(lastCells, currentCell)
             end
 
-            new_line = {tile_x, tile_y, new_tile_x, new_tile_y}
-            table.insert(lines, new_line)
+            local directions = {} --directions: 1 up, 2 right, 3 down, 4 left
 
-            x1 = (tile_x - 1) * tile_size + tile_size * border_space + tile_size / 2
-            y1 = (tile_y - 1) * tile_size + tile_size * border_space + tile_size / 2
-            x2 = (new_tile_x - 1) * tile_size + tile_size * border_space + tile_size / 2
-            y2 = (new_tile_y - 1) * tile_size + tile_size * border_space + tile_size / 2
-            table.insert(draw_lines, {x1, y1, x2, y2})
-
-            new_tile = {new_tile_x, new_tile_y}
-            table.insert(visited_cells, new_tile)
-            table.insert(last_cells, new_tile)
-            tile_x = new_tile_x
-            tile_y = new_tile_y
-
-        else 
-            if #visited_cells < cells_number then --if there are no directions available go back to the previous cell
-                tile_x = last_cells[#last_cells - 1][1]
-                tile_y = last_cells[#last_cells - 1][2]
-                table.remove(last_cells, #last_cells)
+            --add directions based on the coordinates and near cells
+            if currentCell[1] ~= 1 and --add left direction if the cell doesn't touch the left border
+                    not tableContains(visitedCells, {tileX - 1, tileY}) then --and there is no cell on the left
+                table.insert(directions, 4)
             end
-        end
+            if currentCell[1] ~= windowWidth - 2 * borderSpace and --add right direction if the cell doesn't touch the right border
+                    not tableContains(visitedCells, {tileX + 1, tileY}) then --and there is no cell on the right
+                table.insert(directions, 2)
+            end
+            if currentCell[2] ~= 1 and --add up direction if the cell doesn't touch the top border
+                    not tableContains(visitedCells, {tileX, tileY - 1}) then --and there is no cell upper
+                table.insert(directions, 1)
+            end
+            if currentCell[2] ~= windowHeight - 2 * borderSpace and --add down direction if the cell doesn't touch the bottom border
+                    not tableContains(visitedCells, {tileX, tileY + 1}) then --and there is no cell lower
+                table.insert(directions, 3)
+            end
 
-    --the program stops only when all the cells have been filled
-    --draw borders between unconnected cells to make the maze 
-    elseif borders_made == false then
+            if #directions > 0 then --if there is at least one direction, randomly take one of them
 
-        for x = 1, window_width - 2 * border_space, 1 do
-            for y = 1, window_height - 2 * border_space, 1 do
+                local choosenDir = math.random(#directions)
+                local newDir = directions[choosenDir]
 
-                pesto.graphics.setColor(0, 0, 0)
+                local newTileX, newTileY
+                local newLine, newTile
+                local x1, y1, x2, y2
 
-                local border_x, border_y
-                local border_width, border_height
-                local new_border
-                
-                --check if there are lines between the centers of rectangles
-                --if there aren't, in both ways, create a border
-                if (not tableContains(lines, {x, y, x + 1, y})) and (not tableContains(lines, {x + 1, y, x, y})) then --right - left
-                    border_x = tile_size * border_space + x * tile_size - 1
-                    border_y = tile_size * border_space + (y - 1) * tile_size
-                    border_width = border_thickness
-                    border_height = tile_size
-
-                    new_border = {border_x, border_y, border_width, border_height}
-
-                    if not tableContains(borders, new_border) then
-                        table.insert(borders, new_border)
-                    end
+                if newDir == 1 then
+                    newTileY = tileY - 1
+                    newTileX = tileX
+                elseif newDir == 2 then
+                    newTileX = tileX + 1
+                    newTileY = tileY
+                elseif newDir == 3 then
+                    newTileY = tileY + 1
+                    newTileX = tileX
+                elseif newDir == 4 then
+                    newTileX = tileX - 1
+                    newTileY = tileY
                 end
 
-                if (not tableContains(lines, {x, y, x - 1, y})) and (not tableContains(lines, {x - 1, y, x, y})) then --left - right
-                    border_x = tile_size * border_space + (x - 1) * tile_size - 1
-                    border_y = tile_size * border_space + (y - 1) * tile_size
-                    border_width = border_thickness
-                    border_height = tile_size
+                newLine = {tileX, tileY, newTileX, newTileY}
+                table.insert(lines, newLine) --the lines used to make calculations
 
-                    new_border = {border_x, border_y, border_width, border_height}
+                x1 = (tileX - 1) * tileSize + tileSize * borderSpace + tileSize / 2
+                y1 = (tileY - 1) * tileSize + tileSize * borderSpace + tileSize / 2
+                x2 = (newTileX - 1) * tileSize + tileSize * borderSpace + tileSize / 2
+                y2 = (newTileY - 1) * tileSize + tileSize * borderSpace + tileSize / 2
+                table.insert(toDrawLines, {x1, y1, x2, y2}) --the lines that are drawn
 
-                    if not tableContains(borders, new_border) then
-                        table.insert(borders, new_border)
-                    end
+                newTile = {newTileX, newTileY}
+                table.insert(visitedCells, newTile)
+                table.insert(lastCells, newTile)
+                tileX = newTileX
+                tileY = newTileY
+
+            else 
+                if #visitedCells < cellsNumber then --if there are no directions available go back to the previous cell in lastCells
+                    tileX = lastCells[#lastCells - 1][1]
+                    tileY = lastCells[#lastCells - 1][2]
+                    table.remove(lastCells, #lastCells)
                 end
+            end
 
-                if (not tableContains(lines, {x, y, x, y + 1})) and (not tableContains(lines, {x, y + 1, x, y})) then --bottom - up
-                    border_x = tile_size * border_space + (x - 1) * tile_size
-                    border_y = tile_size * border_space + y * tile_size - 1
-                    border_width = tile_size
-                    border_height = border_thickness
+        --the program stops only when all the cells have been filled
+        --draw borders between unconnected cells to make the maze 
+        elseif bordersMade == false then
 
-                    new_border = {border_x, border_y, border_width, border_height}
+            for x = 1, windowWidth - 2 * borderSpace, 1 do
+                for y = 0, windowHeight - 2 * borderSpace, 1 do
 
-                    if not tableContains(borders, new_border) then
-                        table.insert(borders, new_border)
-                    end
+                    pesto.graphics.setColor(0, 0, 0)
 
-                    if (not tableContains(lines, {x, y, x, y - 1})) and (not tableContains(lines, {x, y - 1, x, y})) then --up - bottom
-                        border_x = tile_size * border_space + (x - 1) * tile_size
-                        border_y = tile_size * border_space + (y - 1) * tile_size - 1
-                        border_width = tile_size
-                        border_height = border_thickness
-    
-                        new_border = {border_x, border_y, border_width, border_height}
-    
-                        if not tableContains(borders, new_border) then
-                            table.insert(borders, new_border)
+                    local borderX, borderY
+                    local borderWidth, borderHeight
+                    local newBorder
+                    
+                    --check if there are lines between the centers of rectangles
+                    --if there aren't, in both ways, create a border
+                    if (not tableContains(lines, {x, y, x + 1, y})) and (not tableContains(lines, {x + 1, y, x, y})) then --right - left
+                        borderX = tileSize * borderSpace + x * tileSize - 1
+                        borderY = tileSize * borderSpace + (y - 1) * tileSize
+                        borderWidth = borderThickness
+                        borderHeight = tileSize
+
+                        newBorder = {borderX, borderY, borderWidth, borderHeight}
+
+                        if not tableContains(borders, newBorder) then
+                            table.insert(borders, newBorder)
                         end
                     end
 
-                    borders_made = true
-                    show_lines = false
+                    if (not tableContains(lines, {x, y, x - 1, y})) and (not tableContains(lines, {x - 1, y, x, y})) then --left - right
+                        borderX = tileSize * borderSpace + (x - 1) * tileSize - 1
+                        borderY = tileSize * borderSpace + (y - 1) * tileSize
+                        borderWidth = borderThickness
+                        borderHeight = tileSize
+
+                        newBorder = {borderX, borderY, borderWidth, borderHeight}
+
+                        if not tableContains(borders, newBorder) then
+                            table.insert(borders, newBorder)
+                        end
+                    end
+
+                    if (not tableContains(lines, {x, y, x, y + 1})) and (not tableContains(lines, {x, y + 1, x, y})) then --bottom - up
+                        borderX = tileSize * borderSpace + (x - 1) * tileSize
+                        borderY = tileSize * borderSpace + y * tileSize - 1
+                        borderWidth = tileSize
+                        borderHeight = borderThickness
+
+                        newBorder = {borderX, borderY, borderWidth, borderHeight}
+
+                        if not tableContains(borders, newBorder) then
+                            table.insert(borders, newBorder)
+                        end
+
+                        if (not tableContains(lines, {x, y, x, y - 1})) and (not tableContains(lines, {x, y - 1, x, y})) then --up - bottom
+                            borderX = tileSize * borderSpace + (x - 1) * tileSize
+                            borderY = tileSize * borderSpace + (y - 1) * tileSize - 1
+                            borderWidth = tileSize
+                            borderHeight = borderThickness
+        
+                            newBorder = {borderX, borderY, borderWidth, borderHeight}
+        
+                            if not tableContains(borders, newBorder) then
+                                table.insert(borders, newBorder)
+                            end
+                        end
+
+                        bordersMade = true
+                        showLines = false --once the borders are made, the lines aren't drawn anymore
+                    end
                 end
             end
         end
     end
+
+    --resetting the maze
+    if pesto.mouse.isPressed(0) then
+        tileX = math.random(windowWidth - 2 * borderSpace)
+        tileY = math.random(windowHeight - 2 * borderSpace)
+
+        visitedCells = {}
+        lastCells = {}
+        lines = {}
+        toDrawLines = {}
+        borders = {}
+
+        borderThickness = 2
+        bordersMade = false
+        showLines = true
+    end
+
+    --pausing the maze
+    if pesto.mouse.isPressed(1) then
+        pause = not pause
+    end
+
 end
+
+--grid canvas
+local grid = pesto.graphics.loadCanvas(screenWidth, screenHeight)
+
+grid:beginDrawing()
+
+pesto.graphics.setColor(255, 255, 255)
+
+for i = borderSpace, windowWidth - 1 - borderSpace, 1 do
+    for j = borderSpace, windowHeight - 1 - borderSpace, 1 do
+
+        lineRectangle(i * tileSize, j * tileSize, tileSize, tileSize)
+
+    end
+end
+
+grid:endDrawing()
 
 function pesto.draw()
 
+    pesto.graphics.setColor(255, 255, 255)
+    grid:draw(0, 0)
+
     --color visited cells
-    for i = 1, #visited_cells, 1 do
+    for i = 1, #visitedCells, 1 do
         pesto.graphics.setColor(255, 255, 255)
-        pesto.graphics.rectangle(tile_size * border_space + (visited_cells[i][1] - 1) * tile_size,
-                tile_size * border_space + (visited_cells[i][2] - 1) * tile_size, tile_size, tile_size)
+        pesto.graphics.rectangle(tileSize * borderSpace + (visitedCells[i][1] - 1) * tileSize,
+                tileSize * borderSpace + (visitedCells[i][2] - 1) * tileSize, tileSize, tileSize)
     end
 
     --draw current rectangle till the maze is complete
-    if show_lines then
+    if showLines then
         pesto.graphics.setColor(255, 0, 0)
-        pesto.graphics.rectangle((tile_x - 1) * tile_size + tile_size * border_space,
-                (tile_y - 1) * tile_size + tile_size * border_space, tile_size, tile_size)
+        pesto.graphics.rectangle((tileX - 1) * tileSize + tileSize * borderSpace,
+                (tileY - 1) * tileSize + tileSize * borderSpace, tileSize, tileSize)
     end
 
     --lines that connect the center of connected cells, kinda like the roads you can take
-    if show_lines then
+    if showLines then
         if #lines > 0 then
-            for _, v in pairs(draw_lines) do
+            for _, v in pairs(toDrawLines) do
                 pesto.graphics.setColor(0, 0, 0)
                 pesto.graphics.line(v[1], v[2], v[3], v[4])
             end
@@ -244,7 +293,7 @@ function pesto.draw()
     end
 
     --draw borders
-    if #visited_cells == cells_number and borders_made then
+    if #visitedCells == cellsNumber and bordersMade then
         for _, v in pairs(borders) do
             pesto.graphics.setColor(0, 0, 0)
             pesto.graphics.rectangle(v[1], v[2], v[3], v[4])
